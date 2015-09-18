43fec05e788d494567c8a67bf30a5eccaf820a99
xxycode <245718977@qq.com>
2015-09-18 11:54:11 +0800

    Initial Commit

---
+++ xxyeng/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xxyeng
+//
+//  Created by Xiaoxueyuan on 15/9/18.
+//  Copyright (c) 2015年 Xiaoxueyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
